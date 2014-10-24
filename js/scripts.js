0a220fef1c009651c1ccec45139a72791e67ea5a
BJack <brianjacklinca@gmail.com>
2014-10-24 16:11:43 -0400

    Completed to do
    Everything is working as it should.
    
    Order of operations:
    1. Set global myTable variable
    2. Create the table in a for loop, populate data, stripe table
    3.  Total contributors
    4. Total contribution amount, for loop to iterate through specific cell
    in each row and add amounts then format the output
    
    ### follow up
    parseInt, parseFloat kept throwing NaN in the total contributions amount
    for loop. The .substring(1) is used to get rid of the $ sign formatting
    when we created the table. Is this an acceptable solution? Should this
    be a regEx? The output matches finalpage.jpg, however I want to be sure
    that this solution is correct, or if not how to implement the correct
    solution.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,45 @@
-var myTable = document.getElementById("data");
+//Globals
+	var myTable = document.getElementById("data");
 
-for(var i = 0;i <= firstName.length - 1;i++){
-	var row = myTable.insertRow(i);
+// Create the table
+	for(var i = 0;i <= firstName.length - 1;i++){
+		//insert rows
+			var row = myTable.insertRow(i);
 
-	var cellDate = row.insertCell(0);
-	var cellAmount = row.insertCell(1);
-	var cellFirstName = row.insertCell(2);
-	var cellLastName = row.insertCell(3);
-	var cellAddress = row.insertCell(4);
-		
-	cellDate.innerHTML = date[i] ;
-	cellAmount.innerHTML = "$"+amount[i].toFixed(2);
-	cellFirstName.innerHTML = firstName[i] ;
-	cellLastName.innerHTML = lastName[i] ;
-	cellAddress.innerHTML = street[i] + "<br>" +
-						    city[i] + ", " + 
-						    state[i] + " " +
-						    zip[i] + "<br>";
+		//assign cells
+			var cellDate = row.insertCell(0);
+			var cellAmount = row.insertCell(1);
+			var cellFirstName = row.insertCell(2);
+			var cellLastName = row.insertCell(3);
+			var cellAddress = row.insertCell(4);
 
-	if( i % 2){
-		myTable.rows[i].classList.add("yellowrow");
+		//insert data
+			cellDate.innerHTML = date[i] ;
+			cellAmount.innerHTML = "$"+amount[i].toFixed(2);
+			cellFirstName.innerHTML = firstName[i] ;
+			cellLastName.innerHTML = lastName[i] ;
+			cellAddress.innerHTML = street[i] + "<br>" +
+								    city[i] + ", " + 
+								    state[i] + " " +
+								    zip[i] + "<br>";
+		//Stripe the table						    
+			if( i % 2){
+				myTable.rows[i].classList.add("yellowrow");
+			}
 	}
-}
 
 // total contributors
 	var totalCont = document.getElementById("contributions");
-	var rowCount = myTable.rows.length; 
+	var rowCount = myTable.rows.length;  
 	totalCont.innerHTML = rowCount; 
 
 // total contributions
 	var totalAmount = document.getElementById("totalAmount");	
 
 	for(var i = 0,amount = 0;i <= rowCount - 1;i++){
-		amount += parseFloat(myTable.rows[i].cells[1].innerHTML);
-		totalAmount.innerHTML = "$"+amount.toFixed(2);
+		amount += parseFloat(myTable.rows[i]	       // get row use += to add at each iteration
+								    .cells[1] 		   // get amount cell
+								    .innerHTML 		   // get contents 
+								    .substring(1));    // remove $ sign
+		totalAmount.innerHTML = "$"+amount.toFixed(2); // format and display
 	}
-
-/*
-if(myTable.rows % 2){
-	alert("odd");
-}
-*/
