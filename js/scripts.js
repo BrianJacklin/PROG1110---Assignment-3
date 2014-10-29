fbdbf57565919609403ffb629712a6a90e769a8b
BJack <brianjacklinca@gmail.com>
2014-10-29 15:20:20 -0400

    Cleaning up code
    removing unnecessary whitespace, globalizing vars some so the loops
    don't needlessly iterate through code, reworked some for loop variables
    into the for loop declaration.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
-//Globals
-	var myTable = document.getElementById("data");
+window.onload = function() {
+	//Globals
+	var myTable = document.getElementById("data"),
+	rowCount = myTable.rows,  
+	totalCont = document.getElementById("contributions"),
+	totalAmount = document.getElementById("totalAmount");	
 
-// Create the table
-	for(var i = 0;i <= firstName.length - 1;i++){
-		//insert rows
-			var row = myTable.insertRow(i);
+	function createTable(){
+		// Create the table
+		for(var i = 0, len = firstName.length;i <= len - 1;i++){
+			//insert rows
+			var row = myTable.insertRow(),
 
-		//assign cells
-			var cellDate = row.insertCell(0);
-			var cellAmount = row.insertCell(1);
-			var cellFirstName = row.insertCell(2);
-			var cellLastName = row.insertCell(3);
-			var cellAddress = row.insertCell(4);
+			//assign cells
+			cellDate = row.insertCell(0),
+			cellAmount = row.insertCell(1),
+			cellFirstName = row.insertCell(2),
+			cellLastName = row.insertCell(3),
+			cellAddress = row.insertCell(4);
 
-		//insert data
+			//insert data
 			cellDate.innerHTML = date[i] ;
 			cellAmount.innerHTML = "$"+amount[i].toFixed(2);
 			cellFirstName.innerHTML = firstName[i] ;
@@ -22,24 +27,32 @@
 								    city[i] + ", " + 
 								    state[i] + " " +
 								    zip[i] + "<br>";
-		//Stripe the table						    
+			//Stripe the table						    
 			if( i % 2){
 				myTable.rows[i].classList.add("yellowrow");
 			}
+		}
 	}
 
-// total contributors
-	var totalCont = document.getElementById("contributions");
-	var rowCount = myTable.rows.length;  
-	totalCont.innerHTML = rowCount; 
+	function summary(){
+		// total contributors
+		totalCont.innerHTML = rowCount.length; 
 
-// total contributions
-	var totalAmount = document.getElementById("totalAmount");	
-
-	for(var i = 0,amount = 0;i <= rowCount - 1;i++){
-		amount += parseFloat(myTable.rows[i]	       // get row use += to add at each iteration
-								    .cells[1] 		   // get amount cell
-								    .innerHTML 		   // get contents 
-								    .substring(1));    // remove $ sign
-		totalAmount.innerHTML = "$"+amount.toFixed(2); // format and display
+		// total contributions
+		for(var i = 0,amount = 0, len = rowCount.length;i <= len - 1;i++){
+			amount += parseFloat(myTable.rows[i]	       // get row use += to add at each iteration
+									    .cells[1] 		   // get amount cell
+									    .innerHTML 		   // get contents 
+									    .substring(1));    // remove $ sign
+			totalAmount.innerHTML = "$"+amount.toFixed(2); // format and display
+		}
 	}
+	
+	function init(){
+		createTable();
+		summary();
+	}
+	
+	init();
+}
+	
